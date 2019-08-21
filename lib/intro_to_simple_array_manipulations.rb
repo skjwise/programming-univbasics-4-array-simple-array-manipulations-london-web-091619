745696105688f716f05ba708d49866d9a2cc8ae6
Sarah Wise <skjwise@hotmail.com>
2019-08-21 18:37:51 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,37 @@
+def using_push(array, string)
+  array.push("violet")
+end
+
+def using_unshift(array, string)
+  array.unshift("Staten Island")
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+
+def using_insert(array, element)
+  array.insert(4, "Python")
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+
